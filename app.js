f3b6871988df03ea118925cbfa818c55f35bc2f1
unknown <ferminpachecozapiola@gmail.com>
2021-09-13 22:46:47 -0300

    añadiendo nuevas cosas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(4000, ()=>{
+app.listen(process.env.PORT || 4000, ()=>{
     console.log('Servidor corriendo en http://localhost:4000/');
 });
 
